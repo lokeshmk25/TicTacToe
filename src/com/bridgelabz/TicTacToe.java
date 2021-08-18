a74b75236fed7069330db27eec4a1e1762caceba
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-18 11:19:58 +0530

    Created:Board of char

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -3,5 +3,20 @@ package com.bridgelabz;
 public class TicTacToe {
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe game");
+        GameBoard();
+    }
+
+    public static char[] GameBoard() {
+        //variables
+        char[] board = new char[10];
+
+        //loop iterates for 9 times
+
+        for (int index = 1; index < board.length; index++) {
+            //assingn empty spaces to character
+            board[index] = ' ';
+        }
+        //returning the board
+        return board;
     }
 }
