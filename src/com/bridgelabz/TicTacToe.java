74313821bb09b60da0c49c4504f2b31fd9e78218
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-18 12:07:13 +0530

    Ready to move

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -8,8 +8,8 @@ public class TicTacToe {
         GameBoard();
         players();
         char[][] board = {{' ', ' ', ' '},
-                            {' ', ' ', ' '},
-                            {' ', ' ', ' '}};
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}};
         printBoard(board);
     }
 
@@ -26,26 +26,54 @@ public class TicTacToe {
         //returning the board
         return board;
     }
+
     static void players() {
         char computer;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the option 'X' or 'O' :" );
+        System.out.println("Enter the option 'X' or 'O' :");
         char player1 = scanner.next().charAt(0);
 
-        if(player1 == 'X' ){
+        if (player1 == 'X') {
             computer = 'O';
-        }else{
-            player1 ='O';
+        } else {
+            player1 = 'O';
             computer = 'X';
         }
-        System.out.println("player option is:" +player1);
-        System.out.println("computer is:" +computer);
+        System.out.println("player option is:" + player1);
+        System.out.println("computer is:" + computer);
     }
+
     static void printBoard(char[][] board) {
-        System.out.println(board[0][0] + "|" +  board[0][1] + "|" +  board[0][2] );
+        System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
         System.out.println("-+-+-");
-        System.out.println(board[1][0] + "|" +  board[1][1] + "|" +  board[1][2] );
+        System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
         System.out.println("-+-+-");
-        System.out.println(board[2][0] + "|" +  board[2][1] + "|" +  board[2][2] );
+        System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+    }
+
+    private static boolean isValidMove(char[][] board, String position) {
+        switch (position) {
+            case "1":
+                return (board[0][0] == ' ');
+            case "2":
+                return (board[0][1] == ' ');
+            case "3":
+                return (board[0][2] == ' ');
+            case "4":
+                return (board[1][0] == ' ');
+            case "5":
+                return (board[1][1] == ' ');
+            case "6":
+                return (board[1][2] == ' ');
+            case "7":
+                return (board[2][0] == ' ');
+            case "8":
+                return (board[2][1] == ' ');
+            case "9":
+                return (board[2][2] == ' ');
+            default:
+                return false;
+        }
+
     }
 }
